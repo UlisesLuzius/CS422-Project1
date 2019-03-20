8021ca68b66b7daf838de66d6b312918b579311d
Michal Pleskowicz <plaskacz.panda@gmail.com>
2019-03-20 17:52:22 +0100

    Added the first version of Select.java

--- src/main/java/ch/epfl/dias/ops/volcano/Select.java
+++ src/main/java/ch/epfl/dias/ops/volcano/Select.java
@@ -5,25 +5,62 @@ import ch.epfl.dias.store.row.DBTuple;
 
 public class Select implements VolcanoOperator {
 
-	// TODO: Add required structures
+	private VolcanoOperator child;
+	private BinaryOp op;
+	private int fieldNo;
+	private int value;
 
 	public Select(VolcanoOperator child, BinaryOp op, int fieldNo, int value) {
-		// TODO: Implement
+		this.child = child;
+		this.op = op;
+		this.fieldNo = fieldNo;
+		this.value = value;
 	}
 
 	@Override
 	public void open() {
-		// TODO: Implement
+		child.open();
 	}
 
 	@Override
 	public DBTuple next() {
-		// TODO: Implement
-		return null;
+		DBTuple tuple = child.next();
+
+		if (tuple.eof) {
+			return tuple;
+		}
+
+		int fieldValue = tuple.getFieldAsInt(fieldNo);
+		boolean select = filter(fieldValue, this.op, this.value);
+
+		if (select) {
+			return tuple;
+		} else {
+			return child.next();
+		}
+	}
+
+	public boolean filter(int field, BinaryOp op, int value) {
+		switch (op) {
+		case LT:
+			return field < value;
+		case LE:
+			return field <= value;
+		case EQ:
+			return field == value;
+		case NE:
+			return field != value;
+		case GT:
+			return field > value;
+		case GE:
+			return field >= value;
+		default:
+			return false;
+		}
 	}
 
 	@Override
 	public void close() {
-		// TODO: Implement
+		child.close();
 	}
 }
