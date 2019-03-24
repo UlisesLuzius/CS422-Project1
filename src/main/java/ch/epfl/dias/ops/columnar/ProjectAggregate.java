9c0b1b06df2cc4f54bf2bb5a929c424b24fd9304
Michal Pleskowicz <plaskacz.panda@gmail.com>
2019-03-24 19:03:24 +0100

    Added the first version of ProjectAggregate in comulmnar ops

--- src/main/java/ch/epfl/dias/ops/columnar/ProjectAggregate.java
+++ src/main/java/ch/epfl/dias/ops/columnar/ProjectAggregate.java
@@ -23,16 +23,19 @@ public class ProjectAggregate implements ColumnarOperator {
 		DBColumn column = this.child.execute()[fieldNo];
 		switch (this.agg) {
 		case COUNT:
-			break;
+			return this.count(column);
 		case SUM:
-			break;
+			return this.sum(column);
 		case MIN:
-			break;
+			return this.min(column);
 		case MAX:
-			break;
+			return this.max(column);
 		case AVG:
-			break;
+			return this.avg(column);
+		default:
+			return null;
 		}
+
 	}
 
 	public DBColumn[] count(DBColumn column) {
@@ -43,25 +46,90 @@ public class ProjectAggregate implements ColumnarOperator {
 	}
 
 	public DBColumn[] sum(DBColumn column) {
-
+		DBColumn result = new DBColumn(this.dt);
+		double sum = 0.0;
 		switch (this.dt) {
 		case INT:
-
+			Integer[] intValues = column.getAsInteger();
+			for (int value : intValues) {
+				sum += value;
+			}
+			result.add((int) sum);
 			break;
 		case DOUBLE:
+			Double[] doubleValues = column.getAsDouble();
+			for (double value : doubleValues) {
+				sum += value;
+			}
+			result.add(sum);
 			break;
 		}
+		return new DBColumn[] { result };
 	}
 
 	public DBColumn[] min(DBColumn column) {
-
+		DBColumn result = new DBColumn(this.dt);
+		double min = Double.MAX_VALUE;
+		switch (this.dt) {
+		case INT:
+			Integer[] intValues = column.getAsInteger();
+			for (int value : intValues) {
+				min = Double.min(min, value);
+			}
+			result.add((int) min);
+			break;
+		case DOUBLE:
+			Double[] doubleValues = column.getAsDouble();
+			for (double value : doubleValues) {
+				min = Double.min(min, value);
+			}
+			result.add(min);
+			break;
+		}
+		result.add(min);
+		return new DBColumn[] { result };
 	}
 
 	public DBColumn[] max(DBColumn column) {
-
+		DBColumn result = new DBColumn(this.dt);
+		double max = Double.MIN_VALUE;
+		switch (this.dt) {
+		case INT:
+			Integer[] intValues = column.getAsInteger();
+			for (int value : intValues) {
+				max = Double.max(max, value);
+			}
+			result.add((int) max);
+			break;
+		case DOUBLE:
+			Double[] doubleValues = column.getAsDouble();
+			for (double value : doubleValues) {
+				max = Double.min(max, value);
+			}
+			result.add(max);
+			break;
+		}
+		result.add(max);
+		return new DBColumn[] { result };
 	}
 
 	public DBColumn[] avg(DBColumn column) {
+		DBColumn result = new DBColumn(this.dt);
 
+		DBColumn top = sum(column)[0];
+		DBColumn bottom = count(column)[0];
+
+		double avg = 0.0;
+
+		switch (this.dt) {
+		case INT:
+			avg = (double) top.getAsInteger()[0] / (double) bottom.getAsInteger()[0];
+			break;
+		case DOUBLE:
+			avg = top.getAsDouble()[0] / bottom.getAsDouble()[0];
+			break;
+		}
+		result.add(avg);
+		return new DBColumn[] { result };
 	}
 }
