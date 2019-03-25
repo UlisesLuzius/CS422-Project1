ea2d62ddfb9d99d8c2484c7d17f9bdaa7968264b
Rafael Ulises Luzius <rafael.pizarrosolar@epfl.ch>
2019-03-25 22:16:29 +0100

    DBTuple constructor from 2 tuples

--- src/main/java/ch/epfl/dias/store/row/DBTuple.java
+++ src/main/java/ch/epfl/dias/store/row/DBTuple.java
@@ -12,6 +12,39 @@ public class DBTuple {
 		this.types = types;
 		this.eof = false;
 	}
+	
+	public DBTuple(DBTuple left, DBTuple right, int rightFieldNo) {
+		Object[] leftFields = left.getFields();
+		Object[] rightFields = right.getFields();
+		int leftLength = leftFields.length;
+		int rightLength = rightFields.length;
+
+		DataType[] leftTypes = left.getTypes();
+		DataType[] rightTypes = right.getTypes();
+
+		int finalLength = leftLength + rightLength - 1;
+		Object[] finalFields = new Object[finalLength];
+		DataType[] finalDataTypes = new DataType[finalLength];
+
+
+		for (int i = 0; i < leftLength; i++) {
+			finalFields[i] = leftFields[i];
+			finalDataTypes[i] = leftTypes[i];
+		}
+
+		for (int i = 0; i < rightFieldNo; i++) {
+			finalFields[leftLength + i] = rightFields[i];
+			finalDataTypes[leftLength + i] = rightTypes[i];
+		}
+		for (int i = rightFieldNo + 1; i < rightLength; i++) {
+			finalFields[leftLength + i - 1] = rightFields[i];
+			finalDataTypes[leftLength + i - 1] = rightTypes[i];
+		}
+		
+		this.fields = finalFields;
+		this.types = finalDataTypes;
+		this.eof = false;
+	}
 
 	public DBTuple() {
 		this.eof = true;
