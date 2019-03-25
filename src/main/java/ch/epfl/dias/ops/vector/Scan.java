487caa28659e58873dc8774f608932a3fbf80f52
Michal Pleskowicz <plaskacz.panda@gmail.com>
2019-03-25 16:09:16 +0100

    Import fix in Scan vector

--- src/main/java/ch/epfl/dias/ops/vector/Scan.java
+++ src/main/java/ch/epfl/dias/ops/vector/Scan.java
@@ -1,38 +1,38 @@
 package ch.epfl.dias.ops.vector;
 
 import ch.epfl.dias.store.Store;
+import ch.epfl.dias.store.column.ColumnStore;
 import ch.epfl.dias.store.column.DBColumn;
 
 public class Scan implements VectorOperator {
 
-    private int vectorsize;
-    private int fromIndex;
-    private int toIndex;
-    private ColumnStore store;
+	private int vectorsize;
+	private int fromIndex;
+	private ColumnStore store;
 
-    public Scan(Store store, int vectorsize) {
-        this.store = (ColumnStore) store;
-        this.vectorsize = vectorsize;
-    }
+	public Scan(Store store, int vectorsize) {
+		this.store = (ColumnStore) store;
+		this.vectorsize = vectorsize;
+	}
 
-    @Override
-    public void open() {
-        fromIndex = 0;
-    }
+	@Override
+	public void open() {
+		fromIndex = 0;
+	}
 
-    @Override
-    public DBColumn[] next() {
-        if(fromIndex > store.getColumnSize()) {
-          return null;
-        }
-        int toIndex = Math.min(store.getColumnSize(), fromIndex + vectorsize);
-        DBColumn[] res = store.getColumnsVector(fromIndex, toIndex);
-        fromIndex += vectorsize;
-        return res;
-    }
+	@Override
+	public DBColumn[] next() {
+		if (fromIndex > store.getColumnSize()) {
+			return null;
+		}
+		int toIndex = Math.min(store.getColumnSize(), fromIndex + vectorsize);
+		DBColumn[] res = store.getColumnsVector(fromIndex, toIndex);
+		fromIndex += vectorsize;
+		return res;
+	}
 
-    @Override
-    public void close() {
-        fromIndex = 0;
-    }
+	@Override
+	public void close() {
+		fromIndex = 0;
+	}
 }
